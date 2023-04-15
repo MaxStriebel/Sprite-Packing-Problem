444e7d99bc1387e5f72f92c2b85ea50766f00ed9
Max Striebel <max.striebel@gmail.com>
2023-04-15 16:01:13 +0200

    Added image import and printRandom

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 SOURCE=source/main.c source/pcg_basic.c
 
 build/main: $(SOURCE)
-	cc $(SOURCE) -o build/main
+	cc -Wall -Wno-unused $(SOURCE) -o build/main
 
 run:
 	build/main
--- source/main.c
+++ source/main.c
@@ -3,11 +3,14 @@
 #include <assert.h>
 #include <string.h>
 #include <limits.h>
+#include <stdbool.h>
 
 #include "pcg_basic.h"
+#include "data.h"
 
 #define MIN(A, B) ((A) < (B) ? (A) : (B))
 #define MAX(A, B) ((A) > (B) ? (A) : (B))
+#define ARRAY_COUNT(Array) (sizeof(Array) / sizeof(Array[0]))
 
 typedef struct
 {
@@ -26,6 +29,8 @@ typedef struct
 {
     int ShapeCount;
     shape *Shapes;
+    vector MinOrigin;
+    vector MaxOrigin;
     vector CenterOffset;
     int CellWidth;
     int CellHeight;
@@ -43,6 +48,31 @@ shape shape_allocate(int Width, int Height)
     };
     return Result;
 }
+
+vector vector_add(vector A, vector B)
+{
+    vector Result = {A.X + B.X, A.Y + B.Y};
+    return Result;
+}
+
+vector vector_sub(vector A, vector B)
+{
+    vector Result = {A.X - B.X, A.Y - B.Y};
+    return Result;
+}
+
+vector vector_min(vector A, vector B)
+{
+    vector Result = {MIN(A.X, B.X), MIN(A.Y, B.Y)};
+    return Result;
+}
+
+vector vector_max(vector A, vector B)
+{
+    vector Result = {MAX(A.X, B.X), MAX(A.Y, B.Y)};
+    return Result;
+}
+
 
 problem problem_allocate(int ShapeCount, int MaxExtends)
 {
@@ -63,13 +93,94 @@ problem problem_allocate(int ShapeCount, int MaxExtends)
     return Result;
 }
 
-vector vector_add(vector A, vector B)
+problem problem_createFromShapes(int ShapeCount, shape *Shapes)
 {
-    vector Result = {A.X + B.X, A.Y + B.Y};
+    assert(ShapeCount > 0);
+    int TotalWidth = 0;
+    int TotalHeight = 0;
+    int MaxWidth = 0;
+    int MaxHeight = 0;
+    for(int i = 0; i < ShapeCount; i++)
+    {
+        TotalWidth += Shapes[i].Width;
+        TotalHeight += Shapes[i].Height;
+        MaxWidth = MAX(MaxWidth, Shapes[i].Width);
+        MaxHeight = MAX(MaxHeight, Shapes[i].Height);
+    }
+    vector CenterOffset = {TotalWidth - Shapes[0].Width, 
+                           TotalHeight - Shapes[0].Height};
+    int CellWidth = 2 * TotalWidth - Shapes[0].Width + MaxWidth;
+    int CellHeight = 2 * TotalHeight - Shapes[0].Height + MaxHeight;
+    int CellCount = CellWidth * CellHeight;
+    problem Result = 
+    {
+        .ShapeCount = ShapeCount,
+        .Shapes = Shapes,
+        .MinOrigin = {-CenterOffset.X, -CenterOffset.Y},
+        .MaxOrigin = {TotalWidth, TotalHeight},
+        .CenterOffset = CenterOffset,
+        .CellWidth = CellWidth,
+        .CellHeight = CellHeight,
+        .CellCount = CellCount,
+        .Cells = calloc(CellCount, sizeof (Result.Cells[0]))
+    };
     return Result;
 }
 
-int calculateScore(problem *Problem, vector *Offsets)
+problem problem_createFromIndexes(int Width, int Height, uint8_t *Indexes)
+{
+    int MaxIndex = 0;
+    for(int i = 0; i < Width * Height; i++)
+        MaxIndex = MAX(MaxIndex, Indexes[i]);
+    int ShapeCount = MaxIndex + 1;
+    vector *MinShapes = malloc(ShapeCount * sizeof (vector));
+    vector *MaxShapes = malloc(ShapeCount * sizeof (vector));
+    for(int i = 0; i < ShapeCount; i++)
+    {
+        MinShapes[i] = (vector){INT_MAX, INT_MAX};
+        MaxShapes[i] = (vector){INT_MIN, INT_MIN};
+    }
+    uint8_t *Index = Indexes;
+    for(int y = 0; y < Height; y++)
+    {
+        for(int x = 0; x < Width; x++)
+        {
+            assert(*Index >= 0);
+            assert(*Index < ShapeCount);
+            MinShapes[*Index] = vector_min(MinShapes[*Index], (vector){x, y});
+            MaxShapes[*Index] = vector_max(MaxShapes[*Index], (vector){x, y});
+            Index++;
+        }
+    }
+    shape *Shapes = malloc(ShapeCount * sizeof (shape));
+    for(int i = 0; i < ShapeCount; i++)
+    {
+        vector Size = vector_sub(MaxShapes[i], MinShapes[i]);
+        shape Shape = shape_allocate(Size.X + 1, Size.Y + 1);
+        uint8_t *Cell = Shape.Cells;
+        for(int y = MinShapes[i].Y; y <= MaxShapes[i].Y; y++)
+        {
+            for(int x = MinShapes[i].X; x <= MaxShapes[i].X; x++)
+            {
+                *Cell = Indexes[x + y * Width] == i;
+                Cell++;
+            }
+        }
+        Shapes[i] = Shape;
+    }
+    problem Result = problem_createFromShapes(ShapeCount, Shapes);
+    return Result;
+}
+
+typedef struct
+{
+    int Score;
+    int RawScore;
+    bool Overlap;
+}score;
+
+
+score calculateScore(problem *Problem, vector *Offsets)
 {
     memset(Problem->Cells, 0, Problem->CellCount);
     for(int i = 0; i < Problem->ShapeCount; i++)
@@ -79,6 +190,8 @@ int calculateScore(problem *Problem, vector *Offsets)
         Offset = vector_add(Offset, Problem->CenterOffset);
         uint8_t *TargetLine = Problem->Cells + (Offset.X + Offset.Y * Problem->CellWidth);
         shape Shape = Problem->Shapes[i];
+        assert(Offset.X + Shape.Width <= Problem->CellWidth);
+        assert(Offset.Y + Shape.Height <= Problem->CellHeight);
         uint8_t *SourceCell = Shape.Cells;
         for(int y = 0; y < Shape.Height; y++)
         {
@@ -116,35 +229,73 @@ int calculateScore(problem *Problem, vector *Offsets)
     int Width = MaxX - MinX;
     int Height = MaxY - MinY;
     int Error = MAX(Width, Height) * Overlap;
-    Error = 5 * Overlap;
-    return Width * Height + Error;
+    //Error = 5 * Overlap;
+    score Result =
+    {
+        .Score = Width * Height + Error,
+        .RawScore = Width * Height,
+        .Overlap = Overlap > 0
+    };
+    return Result;
 }
 
-void printGrid(problem *Problem, int Min, int Max)
+void printGrid(problem *Problem)
 {
+    assert(Problem->ShapeCount == 2);
     printf("x, y, score\n");
-    for(int y = Min; y <= Max; y++)
+    for(int y = Problem->MinOrigin.Y; y <= Problem->MaxOrigin.Y; y++)
     {
-        for(int x = Min; x < Max; x++)
+        for(int x = Problem->MinOrigin.X; x <= Problem->MaxOrigin.X; x++)
         {
             vector Point = {x, y};
-            int score = calculateScore(Problem, &Point);
+            int score = calculateScore(Problem, &Point).Score;
             printf("%i, %i, %i\n", x, y, score);
         }
     }
 }
-            
+
+int pcg32_range_r(pcg32_random_t *Rng, int min, int max)
+{
+    int Result = pcg32_boundedrand_r(Rng, max - min) + min;
+    return Result;
+}
+
+void printRandom(problem *Problem, int Iterations, pcg32_random_t *Rng)
+{
+    printf("iteration, score, overlap\n");
+    vector *Point = malloc((Problem->ShapeCount - 1) * sizeof (vector));
+    for(int Iteration = 0; Iteration < Iterations; Iteration++)
+    {
+        for(int ShapeIndex = 1; ShapeIndex < Problem->ShapeCount; ShapeIndex++)
+        {
+            int X = pcg32_range_r(Rng, Problem->MinOrigin.X, Problem->MaxOrigin.X + 1);
+            int Y = pcg32_range_r(Rng, Problem->MinOrigin.Y, Problem->MaxOrigin.Y + 1);
+            Point[ShapeIndex - 1] = (vector){X, Y};
+        }
+        score Score = calculateScore(Problem, Point);
+        printf("%i, %i, %i\n", Iteration, Score.Score, Score.Overlap);
+    }
+}
+
 int main()
 {
-    problem Problem = problem_allocate(2, 10);
+    pcg32_random_t Rng;
+    pcg32_srandom_r(&Rng, 42u, 54u);
+#if 0
     shape Shape0 = {2, 2, (uint8_t[]){1, 1, 1, 0}};
     shape Shape1 = {2, 2, (uint8_t[]){0, 0, 0, 1}};
-    Problem.Shapes = (shape []){Shape0, Shape1};
-    printGrid(&Problem, -8, 8);
+#else
+    shape Shape0 = {3, 4, (uint8_t[]){0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1}};
+    shape Shape1 = {2, 4, (uint8_t[]){1, 1, 1, 1, 1, 0, 1, 1}};
+#endif
+    shape Shapes[] = {Shape0, Shape1};
+
+    //problem Problem = problem_createFromShapes(ARRAY_COUNT(Shapes), Shapes);
+    problem Problem = problem_createFromIndexes(Box_Width, Box_Height, Box);
+    //printGrid(&Problem);
+    printRandom(&Problem, 20000, &Rng);
     return 0;
-    pcg32_random_t rng;
-    pcg32_srandom_r(&rng, 42u, 54u);
     for(int i = 0; i < 10; i++)
-        printf("random: 0x%X\n", pcg32_random_r(&rng));
+        printf("random: 0x%X\n", pcg32_random_r(&Rng));
     return 0;
 }
